107c4a3d3d8728cdfd949ebc5f7a670bb24e69e5
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2021-06-02 13:18:17 +0200

    Add getUser call and user to landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user/user';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-http';
+
+  user: User;
+  users: User;
+  username: string;
+  id: string;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUser('1').subscribe(
+      (res) => {
+        this.user = res[0];
+      },
+      (err) => {
+        this.userService.handleError(err);
+      }
+    );
+  }
 }
